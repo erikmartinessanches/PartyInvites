35e23f88efa6b7108a89db45e9d86ac472dd4f79
Erik Martines Sanches <erik.martinessanches@gmail.com>
2016-06-11 07:18:59 +0200

    Set up an Empty ASP.NET MVC Project, made a Home Controller, some action methods and views.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good afternoon";
+            return View();
+        }
+        public ViewResult Books() {
+            return View();
+        }
+        public RedirectResult Formula() {
+            return Redirect("https://www.rare.co.uk");
+        }
+    }
+}
