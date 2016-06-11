665be328fbc1e962fe6a234505a6fbad70c38d3e
Erik Martines Sanches <erik.martinessanches@gmail.com>
2016-06-11 21:13:52 +0200

    Made a RsvpForm view, a couple of action methods in the HomeController, a Thanks view and a GuestResponse model.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models; /*So I can refer to the GuestResponse model type 
+without needing to qualify the class name.*/
 
 namespace PartyInvites.Controllers
 {
@@ -14,6 +16,22 @@ namespace PartyInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good afternoon";
             return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        { /*The GuestResponse object that is passed as the parameter to the 
+            action method is automatically populated with the data from the 
+            form fields, due to model binding (the names of the input elements
+            are used to set the vaules of the properties in an instance of
+            the model class, which is then passed to the POST-enabled action
+            method).*/
+            
+            //TODO: Email response to the party organizer.
+            return View("Thanks", guestResponse); //Render a specific view.
         }
         public ViewResult Books() {
             return View();
